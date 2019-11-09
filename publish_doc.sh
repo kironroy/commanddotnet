5b228ff25619895300c9842750f641dab16b1b6b
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-09 23:26:58 +0530

    debug

--- publish_doc.sh
+++ publish_doc.sh
@@ -10,7 +10,8 @@ mkdocs build
 echo "documentation generated"
 
 echo "publishing to github"
-git worktree add gh-pages site
+git worktree list
+git worktree add site gh-pages
 cd site
 git add --all
 git commit -m "documentation update"
