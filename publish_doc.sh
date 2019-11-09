4376884c34d2bf8c8b28ce7b18813bd7490e0e25
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-09 23:00:29 +0530

    push manually

--- publish_doc.sh
+++ publish_doc.sh
@@ -5,9 +5,16 @@ pip install --user mkdocs mkdocs-material pygments pymdown-extensions recommonma
 echo "installed"
 
 echo "generating new documentation"
+rm -rf site
 mkdocs build
 echo "documentation generated"
 
 echo "publishing to github"
-mkdocs gh-deploy
+git worktree add site gh-pages
+cd site
+git add --all
+git commit -m "documentation update"
+git push origin gh-pages
+cd ..
+git worktree remove site
 echo "published"
