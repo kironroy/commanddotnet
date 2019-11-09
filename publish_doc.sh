a23ed97f50c89025b82613eb83071591e8bcc955
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-09 22:09:23 +0530

    install deps

--- publish_doc.sh
+++ publish_doc.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
+echo "installing tools"
+pip install mkdocs mkdocs-material pygments pymdown-extensions recommonmark
+echo "installed"
+
 echo "generating new documentation"
+mkdocs build
 echo "documentation generated"
+
 echo "publishing to github"
+
 echo "published"
