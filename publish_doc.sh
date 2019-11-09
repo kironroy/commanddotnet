c5d930fcd2041b9ee46b3a2cde06826b7992621c
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-09 23:47:18 +0530

    push using token

--- publish_doc.sh
+++ publish_doc.sh
@@ -12,11 +12,17 @@ echo "generating new documentation"
 mkdocs build
 echo "documentation generated"
 
+//push setup
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+git config --global push.default current
+
 echo "publishing to github"
 cd site
 git add --all
 git commit -m "documentation update"
-git push origin gh-pages
+git push https://${GITHUB_TOKEN}@github.com/bilal-fazlani/commanddotnet.git origin gh-pages
+# git push origin gh-pages
 cd ..
 git worktree remove site
 echo "published"
